2dd6062c177eef424f6f247de85ee4f16908ee5a
Marco García Carrasco <mgarcar0106@cpifpalanturing.es>
2026-09-18 14:25:13 +0200

    Horario

--- Holamundo/App.java
+++ Holamundo/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) {
+        /* masonada */
         System.out.print("Este metodo no salta de linea\n");
-        System.out.println("Hola Mundo");
+        System.out.println("\033[0m Hola Mundo");
+
+        System.out.printf("El profesor tiene %d años ", 46 );
+        System.out.printf("Ha nacido en %-10s y se crio en %10s\n", "Malaga", "Estepona");
+        System.out.printf("Y vive a %.2f kilometros de distancia", 14.78);
     }
 }
---
+++ Holamundo/Horario.java
@@ -0,0 +1,32 @@
+public class Horario {
+    public static void main(String[] args) {
+        /* 
+        System.out.printf(" %12s Lunes %10s Martes %10s Miercoles %10s Jueves %10s Viernes %10s\n", " ", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ================================================================================================\n");
+        System.out.printf("8.15-9-15%5s IPE%12s END%14s END%17s BAD%14s SIST%10s\n", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ------------------------------------------------------------------------------------------------\n");
+        System.out.printf("9.15-10.15%3s  PROG%11s END%14s PROG%16s BAD%14s SIST%10s\n", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ------------------------------------------------------------------------------------------------\n");
+        System.out.printf("10.15-11.15%2s  PROG%11s END%14s PROG%16s IPE%14s SOST%10s\n", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ================================================================================================\n");
+        System.out.printf("11.15-11.45%10s  %20sRECREO%20s\n", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ================================================================================================\n");
+        System.out.printf("11.45-12.45%2s  SIST%11s BAD%14s PROG%16s IPE%14s PROG%10s\n", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ------------------------------------------------------------------------------------------------\n");
+        System.out.printf("12.45-13.45%2s  SIST%11s BAD%14s BAD%17s LENG%13s PROG%10s\n", " ", " ", " ", " ", " ", " ");
+        System.out.printf(" ------------------------------------------------------------------------------------------------\n");
+        System.out.printf("13.45-14.45%2s  SIST%10s LENG%14s BAD%17s LENG%13s PROG%10s\n", " ", " ", " ", " ", " ", " ");
+       */
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "LUNES ", "MARTES ", "MIÉRCOLES ", "JUEVES ", "VIERNES ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "IPE1 ", "EDES ", "EDES ", "BAD ", "SIST ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "PROG ", "EDES ", "PROG ", "BAD ", "SIST ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "PROG ", "EDES ", "PROG ", "IPE1 ", "SOST ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "RECREO ", "RECREO ", "RECREO ", "RECREO ", "RECREO ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "SIST ", "BAD ", "PROF ", "IPE1 ", "PROG ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "SIST ", "BAD ", "BAD ", "LENG ", "PROG ");
+        System.out.printf(" |%10s|%10s|%10s|%10s|%10s|\n", "SIST ", "BAD ", "BAD ", "LENG ", "PROG ");        
+
+       
+    }
+    
+}
